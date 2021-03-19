efde29239b6976ddd47b442d3a61ac208b0b11b1
yohesky <yoheskyjpp@gmail.com>
2021-03-19 16:48:50 -0400

    cambio de url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   API_URL: "https://api.football-data.org/v2/",
-  API_SERVER: "http://localhost:3000/api/",
-  // API_SERVER: "https://api-futbol.herokuapp.com/api/",
+  // API_SERVER: "http://localhost:3000/api/",
+  API_SERVER: "https://api-futbol.herokuapp.com/api/",
   tokenFotbal: "9e5a04e4f8d14f43b57dd8cafcbb86a7"
 };
 
